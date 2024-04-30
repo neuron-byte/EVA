7d6b20be8f068955d3396cf17cc257166f67a40c
Jhonatas Flor <jhonatas.flor@dcx.ufpb.br>
2024-04-30 04:36:49 -0300

    preset

--- eva/eva.ino
+++ eva/eva.ino
@@ -13,7 +13,14 @@
 IPAddress apIP(192,168,4,1);
 IPAddress subnet(255,255,255,0);
 
+// L298N
+#define IN1_A 14
+#define IN2_A 13
+#define IN3_B 12
+#define IN4_B 11 
 
+byte moves[8] = {0,1,0,1,0,2,0,1};
+byte s = 0;
 Zinu *zinu;
 camera_fb_t* frameBuffer = NULL;
 
@@ -42,8 +49,58 @@ void blink_led(int pin) {
   digitalWrite(pin, LOW);
 }
 
+void move_forward(){
+    digitalWrite(IN1_A, HIGH);
+    digitalWrite(IN2_A, LOW);
+    digitalWrite(IN3_B, HIGH);
+    digitalWrite(IN4_B, LOW);
+}
+
+void move_back(){
+    digitalWrite(IN1_A, LOW);
+    digitalWrite(IN2_A, HIGH);
+    digitalWrite(IN3_B, LOW);
+    digitalWrite(IN4_B, HIGH);
+}
+
+void turn_right(){
+    digitalWrite(IN1_A, HIGH);
+    digitalWrite(IN2_A, LOW);
+    digitalWrite(IN3_B, LOW);
+    digitalWrite(IN4_B, HIGH);
+}
+
+void turn_left(){
+    digitalWrite(IN1_A, LOW);
+    digitalWrite(IN2_A, HIGH);
+    digitalWrite(IN3_B, HIGH);
+    digitalWrite(IN4_B, LOW);
+}
+
+
+void moving(){
+  switch (moves[s])
+  {
+  case 0:
+    move_forward();
+    break;
+  case 1:
+    turn_left();
+    break;
+  case 2:
+    turn_right();
+    break;
+  }
+
+  s = s==7 ? 0 : s++;
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(IN1_A, OUTPUT);
+  pinMode(IN2_A, OUTPUT);
+  pinMode(IN3_B, OUTPUT);
+  pinMode(IN4_B, OUTPUT);
 
   Serial.begin(115200);
 
@@ -75,21 +132,23 @@ void loop() {
   zinu->checkIncomingSignal();
   switch (zinu->state) {
     case DATA_REQUEST:
+      moving();
       blink_led(LED_BUILTIN);
-      Serial.println("Iniciando camera.");
-      Serial.println("Camera iniciada.");
-      Serial.println("Capturando Frame");
+      //Serial.println("Iniciando camera.");
+      //Serial.println("Camera iniciada.");
+      //Serial.println("Capturando Frame");
       flashLight(255);
       frameBuffer = esp_camera_fb_get();
       if(!frameBuffer){
-        Serial.println("Error: Falha ao capturar frame");
+        //Serial.println("Error: Falha ao capturar frame");
         flashLight(0);
         break;
       }
       zinu->state = SENDING_DATA;
+      
       break;
     case SENDING_DATA:
-      Serial.println("Enviando dados");
+      //Serial.println("Enviando dados");
       blink_led(LED_BUILTIN);
       zinu->sendData(frameBuffer->buf, frameBuffer->len);
       if(frameBuffer){
